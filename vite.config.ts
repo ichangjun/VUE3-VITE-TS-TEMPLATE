7ad55bf1e0be7b257d91cd3f2439d5f13617c775
changjun <anson1992@163.com>
2024-01-05 15:57:23 +0800

    完善文档，修改tsconfig.json文件配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: changjun anson1992@163.com
+ * @Date: 2024-01-05 10:36:01
+ * @LastEditors: changjun anson1992@163.com
+ * @LastEditTime: 2024-01-05 15:51:42
+ * @FilePath: /VUE3-VITE-TS-TEMPLATE/vite.config.ts
+ * @Description: 工程配置文件
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
