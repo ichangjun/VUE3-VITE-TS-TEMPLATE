f52345cb1972cb9944c7ea1a34b3ecda5c5dc2e0
changjun <anson1992@163.com>
2024-01-05 15:57:59 +0800

    添加文档注释

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: changjun anson1992@163.com
+ * @Date: 2024-01-05 10:36:01
+ * @LastEditors: changjun anson1992@163.com
+ * @LastEditTime: 2024-01-05 15:57:25
+ * @FilePath: /VUE3-VITE-TS-TEMPLATE/src/App.vue
+ * @Description: 工程入口文件
+-->
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 </script>
@@ -21,9 +29,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
