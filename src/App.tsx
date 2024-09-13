465f758115b1670456ebc7d77cad1329851e0fd0
changjun19920716@gmail.com <changjun199261>
2024-09-13 10:36:54 +0800

    💚 优化代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 /*
  * @Author: changjun anson1992@163.com
  * @Date: 2024-01-05 10:36:01
- * @LastEditors: changjun anson1992@163.com
- * @LastEditTime: 2024-01-10 18:26:06
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2024-09-13 10:31:22
  * @FilePath: /VUE3-VITE-TS-TEMPLATE/src/App.tsx
  * @Description: 工程入口文件
  */
-import { defineComponent } from 'vue';
-import Logo from '@/assets/images/logo.jpg';
+import { defineComponent } from 'vue'
+import Logo from '@/assets/images/logo.jpg'
 export default defineComponent({
   name: 'App',
   setup() {
     return () => (
-      <div id="app">
+      <div id='app'>
         <i-echarts />
         <i-svg-icon />
         <img src={Logo} />
@@ -22,4 +22,3 @@ export default defineComponent({
     )
   }
 })
-
